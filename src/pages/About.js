4f3b655560cd338bc7d21a2217ea6b57ea3b2a98
unknown <victorynwawunze@gmail.com>
2020-10-28 02:09:39 -0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and
+        typesetting industry Lorem Ipsum has been the industry's standard dummy
+        text ever since the 1500s when an unknown printer took a galley of type
+        and scrambled it to make a type specimen book it has?
+      </p>
+    </section>
+  );
+};
+
+export default About;
